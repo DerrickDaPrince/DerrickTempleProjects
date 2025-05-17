4c5e95a5484ba3716008744d717ea6b63c0b0192
Derrick.Wilkerson <derrickwilkerson09@yahoo.com>
2025-05-17 14:38:58 -0500

    first commit

---
+++ DecisionsAtTheCrossRoad.py
@@ -0,0 +1,9 @@
+#Task 1 
+number = float(input("Enter a number: "))  # Convert input to number (int or float)
+
+if number > 0:
+    print("The number is positive.")
+elif number == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
--- Python Naming Convention Pracice.py
+++
@@ -1,5 +0,0 @@
-#Question 1 Task 1
-PI_VALUE = 3.14
-user_age = 25
-user_location = "New York"
-MAX_LIMIT = 1000
